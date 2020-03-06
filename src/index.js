785aff69f225a96bb73ed0adeca1b7381d71b987
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-06 22:21:28 +0300

    Создаёт структуру файлов и папок для компонентов проекта

--- src/index.js
+++ src/index.js
@@ -1,40 +1,6 @@
-class SwapiService {
-  _apiBase = 'https://swapi.co/api';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, received ${res.status}`);
-    }
-
-    return res.json();
-  };
-
-  async getAllPeople() {
-    const res = await this.getResource(`/people/`);
-    return res.results;
-  }
-
-  async getPerson(id) {
-    return this.getResource(`/people/${id}`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResource(`/planets/`);
-    return res.results;
-  }
-
-  async getPlanet(id) {
-    return this.getResource(`/planets/${id}`);
-  }
-
-  async getAllStarships() {
-    const res = await this.getResource(`/starships/`);
-    return res.results;
-  }
-
-  async getStarship(id) {
-    return this.getResource(`/starships/${id}`);
-  }
-}
+ReactDOM.render(<App />,
+  document.getElementById('root'));
---
+++ src/services/swapi-service.js
@@ -0,0 +1,41 @@
+export default class SwapiService {
+  _apiBase = 'https://swapi.co/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}
+        , received ${res.status}`);
+    }
+
+    return res.json();
+  };
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  }
+
+  async getPerson(id) {
+    return this.getResource(`/people/${id}`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  }
+
+  async getPlanet(id) {
+    return this.getResource(`/planets/${id}`);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
+
+  async getStarship(id) {
+    return this.getResource(`/starships/${id}`);
+  }
+}
