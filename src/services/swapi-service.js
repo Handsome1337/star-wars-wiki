d249556468b2632802bc5fbc05804bff70d0e3ad
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-07 21:01:51 +0300

    Реализует отображение списка персонажей в ItemList

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -47,7 +47,7 @@ export default class SwapiService {
     return item.url.match(idRegExp)[1];
   }
 
-  _transformPerson(person) {
+  _transformPerson = (person) => {
     return {
       id: this._extractId(person),
       name: person.name,
@@ -57,7 +57,7 @@ export default class SwapiService {
     };
   }
 
-  _transformPlanet(planet) {
+  _transformPlanet = (planet) => {
     return {
       id: this._extractId(planet),
       name: planet.name,
@@ -67,7 +67,7 @@ export default class SwapiService {
     };
   }
 
-  _transformStarship(starship) {
+  _transformStarship = (starship) => {
     return {
       id: this._extractId(starship),
       name: starship.name,
