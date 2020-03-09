bb511acf5e23371f5d30660c95caaa84858c0038
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-09 12:14:41 +0300

    Рефакторинг SwapiService, PeoplePage и ItemList

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 
 import Spinner from './../spinner';
-import SwapiService from './../../services/swapi-service';
 
 import './item-list.css';
 
 export default class ItemList extends Component {
 
-  swapiService = new SwapiService();
-
   state = {
-    peopleList: null
+    itemList: null
   }
 
   componentDidMount() {
-    this.swapiService
-      .getAllPeople()
-      .then((peopleList) => {
+    const { getData } = this.props;
+
+    getData()
+      .then((itemList) => {
         this.setState({
-          peopleList
+          itemList
          })
       });
   }
@@ -36,13 +34,13 @@ export default class ItemList extends Component {
   }
 
   render() {
-    const { peopleList } = this.state;
+    const { itemList } = this.state;
 
-    if (!peopleList) {
+    if (!itemList) {
       return <Spinner />;
     }
 
-    const items = this.renderItems(peopleList);
+    const items = this.renderItems(itemList);
 
     return (
       <ul className="item-list list-group">
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 import ErrorIndicator from'./../error-indicator';
 import ItemList from './../item-list';
 import PersonDetails from './../person-details';
+import SwapiService from './../../services/swapi-service';
 
 export default class PeoplePage extends Component {
+
+  swapiService = new SwapiService();
 
   state = {
     selectedPerson: null,
@@ -32,7 +35,7 @@ export default class PeoplePage extends Component {
     return (
       <div className="row mb2">
         <div className="col-md-6">
-          <ItemList onItemSelected={this.onPersonSelected} />
+          <ItemList getData={this.swapiService.getAllPeople} onItemSelected={this.onPersonSelected} />
         </div>
         <div className="col-md-6">
           <PersonDetails personId={this.state.selectedPerson} />
