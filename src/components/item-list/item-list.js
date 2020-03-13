f9f1bbc29bb58741cac39c2d4477b134c3425e3b
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-13 16:13:43 +0300

    Устанавливает библиотеку prop-types

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './item-list.css';
 
@@ -23,6 +24,16 @@ const ItemList = (props) => {
       { items }
     </ul>
   );
+};
+
+ItemList.defaultProps = {
+  onItemSelected: () => {}
+};
+
+ItemList.propPypes = {
+  onItemSelected: PropTypes.func,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  children: PropTypes.func.isRequired
 };
 
 export default ItemList;
